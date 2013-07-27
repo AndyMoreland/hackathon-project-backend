14996c113a4be95e8529c8167c0be8082a046ec1
Andy Moreland <andy.moreland@airbnb.com>
2013-07-27 12:58:36 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HackathonProjectBackend::Application.config.secret_key_base = '0049e89caa199403509711b906bbdb3c9e4e431e41eb36f9e393fb5ea24f5ee440c4f9ac684afdab01bd311aa0242a7bd1864ae35a74874979e73d06ace669d7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HackathonProjectBackend::Application.config.session_store :cookie_store, key: '_hackathon-project-backend_session'
