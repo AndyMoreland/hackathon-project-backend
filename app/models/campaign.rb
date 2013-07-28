f5c8b72b8012758ec3cfb5560d22bcf94bd0b01b
Kyle Zemek <kyle.zemek@airbnb.com>
2013-07-27 23:48:19 -0700

    defaults

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -3,4 +3,14 @@ class Campaign < ActiveRecord::Base
   has_many :metrics
 
   scope :published, -> { where(:published => true) }
+  after_initialize :init
+
+  def init
+    self.split  ||= 100
+    self.app_id ||= 1
+    self.test_a ||= "Control"
+    self.test_b ||= "Test"
+    self.published ||= false
+    self.locked ||= false
+  end
 end
