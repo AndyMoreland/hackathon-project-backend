4d773eb61f6083c5d3c601de743886a7356b31cb
Eli White <eli.white@airbnb.com>
2013-07-28 05:36:09 -0700

    Stats displays conversions

--- app/assets/javascripts/statics/views/stats.js
+++ app/assets/javascripts/statics/views/stats.js
@@ -9,6 +9,25 @@
     return vals;
   }
 
+  function formatData(mixData) {
+    obj = [];
+
+    for (var key in mixData) {
+      obj.push({
+        date: key,
+        value: mixData[key]
+      });
+    }
+
+    return obj;
+  }
+
+  function extend(data, percent) {
+    var diff = data[1] - data[0];
+    var amount = diff * percent;
+    return [data[0] - .5 * amount, data[1] + .5 * amount];
+  }
+
   var StatsView = Backbone.View.extend({
     template: HandlebarsTemplates['stats'],
     className: "stats-page",
@@ -33,12 +52,9 @@
           console.log(values);
 
           // define dimensions of graph
-          var m = [80, 80, 80, 80]; // margins
-          var w = 1000 - m[1] - m[3]; // width
+          var m = [0, 0, 30, 60]; // margins
+          var w = 800 - m[1] - m[3]; // width
           var h = 400 - m[0] - m[2]; // height
-
-          // create a simple data array that we'll plot with a line (this array represents only the Y values, X will just be the index location)
-          var data = [3, 6, 2, 7, 5, 2, 0, 3, 8, 9, 2, 5, 9, 3, 6, 3, 6, 2, 7, 5, 2, 1, 3, 8, 9, 2, 5, 9, 2, 7];
 
           // X scale will fit all values from data[] within pixels 0-w
           var x_time = d3.time.scale().domain(
@@ -51,15 +67,9 @@
           var domain = [d3.min(domain_array), d3.max(domain_array)];
 
           var y_time = d3.scale.linear().domain(
-            domain
+            extend(domain, .10)
           )
             .range([h, 0]);
-
-          var x = d3.scale.linear().domain([0, data.length]).range([0, w]);
-          // Y scale will fit values from 0-10 within pixels h-0 (Note the inverted domain for the y-scale: bigger is up!)
-          var y = d3.scale.linear().domain([0, 10]).range([h, 0]);
-          // automatically determining max range can work something like this
-          // var y = d3.scale.linear().domain([0, d3.max(data)]).range([h, 0]);
 
           // create a line function that can convert data[] into x and y points
           var line = d3.svg.line()
@@ -68,26 +78,26 @@
             // verbose logging to show what's actually being done
             //console.log('Plotting X value for data point: ' + d + ' using index: ' + i + ' to be at: ' + x(i) + ' using our xScale.');
             // return the X coordinate where we want to plot this datapoint
-            return x(i);
+            return x_time(new Date(d.date));
           })
             .y(function(d) {
               // verbose logging to show what's actually being done
               //console.log('Plotting Y value for data point: ' + d + ' to be at: ' + y(d) + " using our yScale.");
               // return the Y coordinate where we want to plot this datapoint
-              return y(d);
+              return y_time(d.value);
             })
 
           var findNode = $(this.el).find("#graph")[0];
 
           // Add an SVG element with the desired dimensions and margin.
           var graph = d3.select(findNode).append("svg:svg")
-            .attr("width", w + m[1] + m[3])
+            .attr("width", w + m[1]+m[3])
             .attr("height", h + m[0] + m[2])
             .append("svg:g")
             .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
 
           // create yAxis
-          var xAxis = d3.svg.axis().scale(x).tickSize(-h).tickSubdivide(true);
+          var xAxis = d3.svg.axis().scale(x_time).tickSize(-h).tickSubdivide(true).orient("bottom");
           // Add the x-axis.
           graph.append("svg:g")
             .attr("class", "x axis")
@@ -96,16 +106,15 @@
 
 
           // create left yAxis
-          var yAxisLeft = d3.svg.axis().scale(y).ticks(4).orient("left");
+          var yAxisLeft = d3.svg.axis().scale(y_time).ticks(8).orient("left");
           // Add the y-axis to the left
           graph.append("svg:g")
             .attr("class", "y axis")
-            .attr("transform", "translate(-25,0)")
             .call(yAxisLeft);
 
           // Add the line by appending an svg:path element with the data line we created above
           // do this AFTER the axes above so that the line is above the tick-lines
-          graph.append("svg:path").attr("d", line(data));
+          graph.append("svg:path").attr("d", line(formatData(values.values.B)));
         }, this)
       );
 
